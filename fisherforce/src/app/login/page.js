ec591e86dfc31cb21fdc4705847b4c7a50ee09b8
FisherForce <thaolekana9@gmail.com>
2026-01-21 22:08:07 +0100

    Update page.js

--- fisherforce/src/app/login/page.js
+++ fisherforce/src/app/login/page.js
@@ -1,19 +1,36 @@
 "use client";
 
+import { useState } from "react";
 import { supabase } from "../../lib/supabase";
 
 export default function Login() {
-  async function login() {
-await supabase.auth.signInWithOtp({
-  email
-});
+  const [email, setEmail] = useState("");
+  const [sent, setSent] = useState(false);
 
+  async function login() {
+    await supabase.auth.signInWithOtp({
+      email
+    });
+    setSent(true);
   }
 
   return (
     <div className="center">
       <h1>Connexion Fisherforce</h1>
-      <button onClick={login}>Se connecter avec Google</button>
+
+      {!sent ? (
+        <>
+          <input
+            type="email"
+            placeholder="Ton email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <button onClick={login}>Recevoir le lien</button>
+        </>
+      ) : (
+        <p>📩 Un lien de connexion t’a été envoyé par email</p>
+      )}
     </div>
   );
 }
